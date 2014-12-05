4043a571618e6126b64c600ada0a66ea40be6821
KeSaga <beijifeng_saga@163.com>
2014-12-05 18:31:16 +0800

    增加TreeViewItem和ListViewItem使用控件模板和数据模板的例子

---
+++ WPFControls/ControlTemplates/TableTreeViewItemCtrTmp.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFControls.ControlTemplates
+{
+    public partial class TableTreeViewItemCtrTmp : ResourceDictionary
+    {
+
+        public TableTreeViewItemCtrTmp()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            ContextMenu cntMenu = this["SubItemListCntMenu"] as ContextMenu;
+            if (btn == null || cntMenu == null) return;
+            //定义菜单显示的位置（相对于目标的位置）
+            cntMenu.PlacementTarget = btn;
+            cntMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+            cntMenu.IsOpen = true;
+        }
+
+    }
+}
---
+++ WpfTest/ExpandLisstBoxItemTestUserCtr.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTest
+{
+    /// <summary>
+    /// ExpandLisstBoxItemTestUserCtr.xaml 的交互逻辑
+    /// </summary>
+    public partial class ExpandLisstBoxItemTestUserCtr : UserControl
+    {
+        public ExpandLisstBoxItemTestUserCtr()
+        {
+            InitializeComponent();
+
+            ResourceDictionary resourceDic = new ResourceDictionary();
+            resourceDic.Source = new Uri(
+                "WPFControls;component/DataTemplates/ExpandListBoxItemDtTmp.xaml", UriKind.Relative);
+            this.ltBoxDataSource.ItemTemplate = (DataTemplate)resourceDic["ExpandListBoxItemDataTemplate"];
+            this.ltBoxValues.ItemTemplate = (DataTemplate)resourceDic["ExpandListBoxItemDataTemplate"];
+
+            HAHA h = new HAHA();
+            h.ObjectName = "ads";
+            h.Description = "uiuiasdnsoihsauiuiasdnsoihsauiuiasdnsoihsauiuiasdnsoihsauiuiasdnsoihsauiuiasdnsoihsauiuiasdnsoihsauiuiasdnsoihsauiuiasdnsoihsauiuiasdnsoihsa";
+            List<HAHA> HL = new List<HAHA>();
+            for (int i = 0; i < 2; i++)
+                HL.Add(h);
+            this.ltBoxDataSource.ItemsSource = HL;
+        }
+
+        private void btnDataSourcesEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSearchDataSource_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddDataSource_Click(object sender, RoutedEventArgs e)
+        {
+            grdDataSourcesEdit.Visibility = Visibility.Visible;
+        }
+
+        private void btnDataSourcesOK_Click(object sender, RoutedEventArgs e)
+        {
+            grdDataSourcesEdit.Visibility = Visibility.Collapsed;
+        }
+
+        private void btnValuesOK_Click(object sender, RoutedEventArgs e)
+        {
+            grdValuesEdit.Visibility = Visibility.Collapsed;
+        }
+
+        private void btnAddValue_Click(object sender, RoutedEventArgs e)
+        {
+            grdValuesEdit.Visibility = Visibility.Visible;
+        }
+
+        private void btnDeleteValues_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEditValues_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDeleteDataSources_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSearchValue_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+
+    public class HAHA
+    {
+        public string ObjectName { get; set; }
+        public string Description { get; set; }
+    }
+
+}
